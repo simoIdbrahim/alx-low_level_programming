8b133136da5402ed64842148c07dbf6ad25f0b38
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-06-15 16:48:55 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly and that piece of
+ * art is useful\"
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
- * Description: A C program that prints with put function
- * 	ssize_t write(int fd. const void *buf, size_t count);
- *
- * Return: Alawys 1 (error).
-*/
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1, quo, 59);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
