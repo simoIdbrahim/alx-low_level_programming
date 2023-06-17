2157effe5f2bd95a6d31abad4d40ebbed9daf8fd
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-06-16 17:08:48 -0700

    prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible different
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0, digit2;
+
+	while (digit <= 9)
+	{
+		digit = 0;
+		while (digit2 <= 9)
+		{
+			if (digit != digit2 && digit < digit2)
+			{
+				putchar(digit + 48);
+				putchat(digit2 + 48);
+
+				if (digit + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
