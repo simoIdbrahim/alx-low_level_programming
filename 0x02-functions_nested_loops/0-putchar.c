cb286cb43b454e89fbd9ffbd1f6189f0b7beddd4
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-06-20 11:10:16 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h";
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for(ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar("\n");
+
+	return (0);
+}
