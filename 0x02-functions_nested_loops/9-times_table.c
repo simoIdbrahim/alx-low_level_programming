f29be783abad4a4c1ba81ae2479a5f998fff98b5
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-06-20 15:50:16 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,19 +13,23 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
+		_putchar(48);
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((prod / 10) + 48); /*get the first digit*/
 
-		_putchar((prod % 10) + 48); /*get the second digti*/
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get the first digit*/
+
+			_putchar((prod % 10) + 48); /*get the second digti*/
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
