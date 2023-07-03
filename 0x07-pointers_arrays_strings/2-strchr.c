6e6261142dee6a58eccdf65cfff65146f29cbe58
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-03 15:42:27 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - main function
+ * @s: pointer to put the constant
+ * @c: constant
+ * @n: length of src to be copied
+ *
+ * Return: pointer to s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0' ; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return ('\0');
+}
