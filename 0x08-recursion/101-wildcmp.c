03725bd951e6050f439e082f72e7ea2f72413b28
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-05 09:38:03 -0700

    101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+/**
+ * move_past_star - main function
+ * @s2: the second string, can contain wildcard
+ * Return: the pointer past star
+ */
+
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	else
+		return (s2);
+}
+
+/**
+ * inception - main function
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: 1 if identical, 0 if false
+ */
+
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
+
+/**
+ * wildcmp - main function
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if identical, 0 if false
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
