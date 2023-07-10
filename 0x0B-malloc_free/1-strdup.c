253976355d0ece7c1ac410400ee4518f7d04df06
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-10 08:16:42 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strdup - main function.
+ * @str: stirng.
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+	;
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{
+
+		return (NULL);
+	}
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
