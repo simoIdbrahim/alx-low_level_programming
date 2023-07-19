e2d61a3ac4d1deb7a9fc7e02b66610f638195b0b
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-19 14:50:32 -0700

    main_opcodes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define FUNCTION_POINTERS_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
