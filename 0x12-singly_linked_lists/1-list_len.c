2f4bfff2891f99a26b2d778d918d409a4993eeff
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-28 07:47:23 -0700

    add node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,21 @@
 
-
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
+ * list_len - the main function
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		n++;
+		num++;
 		h = h->next;
 	}
-	return (n);
+	return (num);
 }
-
