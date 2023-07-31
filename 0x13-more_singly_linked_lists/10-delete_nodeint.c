adbcb978a06d27cf4cf284d76eb70e81d7302b72
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-31 11:50:53 -0700

    delete index node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *current = NULL;
-	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -24,14 +23,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < index - 1)
+	for (i = 0; i < index - 1; i++)
 	{
 		if (!temp || !(temp->next))
-			return (-1);
-		temp = temp->next;
-		i++;
+                        return (-1);
+                temp = temp->next;
 	}
-
 
 	current = temp->next;
 	temp->next = current->next;
