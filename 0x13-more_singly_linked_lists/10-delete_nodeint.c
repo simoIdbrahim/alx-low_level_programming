b7d09c9395d38c86250de308d7f75f925cc797e0
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-31 11:36:27 -0700

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - main function
+ * @head: argaddress pointer firts el
+ * @index: position index delete
+ *
+ * Return: 1 || -1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node = *head;
+	listint_t *curr = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+
+	for (i < index  - 1; i++;)
+	{
+		if (!node || !(node->next))
+			return (-1);
+		node = node->next;
+	}
+
+	curr = node->next;
+	node->next = curr->next;
+	free(curr);
+
+	return (1);
+}
+
