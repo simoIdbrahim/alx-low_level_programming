590cde21a2b5e51640fb0be74ee41deee154683e
simoIdbrahim <mohamedidbrahim16@gmail.com>
2023-07-31 09:59:22 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_listint - main function
+ * @h: poiner to node
+ *
+ * Return: size int.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
+
